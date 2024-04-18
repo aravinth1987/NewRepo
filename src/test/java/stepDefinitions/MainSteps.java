b9f576484c7de0526e7c1f2160a7bf7674f1b260
aravinth <aravinth.annauniv@gmail.com>
2024-04-18 13:10:55 -0400

    commit from IST time zone user

--- src/test/java/stepDefinitions/MainSteps.java
+++ src/test/java/stepDefinitions/MainSteps.java
@@ -11,6 +11,11 @@ public class MainSteps {
 	public void user_is_on_net_banking_landing_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    System.out.println("User is on NetBanking landing page");
+	    System.out.println("Change 1  from IST user");
+	    System.out.println("Change 2  from IST user");
+	    System.out.println("Change 3  from IST user");
+
+	    
 	}
 	
 //	@When("user login to application with {string} and {string}")
